79101c40e7f8162258391d698c28f4c72d98532c
Daniel Hernandez <danielmark252525@gmail.com>
2019-09-02 14:12:15 -0400

    Adds the pre ran user migration and updated #49 Add gems 9/2/2019

---
+++ db/migrate/20190902175411_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :preferred_name
+      t.string :user_role, null: false
+      t.string :pronunciation
+
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :session_token , unique: true
+  end
+end
