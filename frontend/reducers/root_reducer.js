29e795821f57969a8fde8e8849a8c78456f0e906
Daniel Hernandez <danielmark252525@gmail.com>
2019-09-02 17:40:46 -0400

    Adds the store and the root reducer

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+import entitiesReducer from './entities_reducer';
+import sessionReducer from './session_reducer.js';
+import errorsReducer from './errors_reducer.js';
+
+
+const rootReducer = combineReducers({
+    entities: entitiesReducer,
+    session: sessionReducer,
+    errors: errorsReducer
+});
+
+export default rootReducer;
