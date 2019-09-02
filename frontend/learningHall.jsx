7c1e1840e33a1ad71d2e8d89d84e0db2b4b4c2dc
Daniel Hernandez <danielmark252525@gmail.com>
2019-09-02 17:39:05 -0400

    Adds the user auth for the home page

--- frontend/learningHall.jsx
+++ frontend/learningHall.jsx
@@ -1,1 +1,28 @@
-// npm install --save webpack webpack-cli react react-dom react-router-dom redux react-redux @babel/core @babel/preset-react @babel/preset-env babel-loader
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Root from './components/root';
+import configureStore from './store/store';
+document.addEventListener("DOMContentLoaded", () => {
+
+    
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
+
+
+    const store = configureStore()
+    const root = document.getElementById("root")
+    ReactDOM.render(<Root  store={store}/>, root)
+})
