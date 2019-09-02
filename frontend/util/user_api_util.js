bbd48ac3a32cb4ceb1e9a9b2181ab5cec9fd30a6
Daniel Hernandez <danielmark252525@gmail.com>
2019-09-02 17:43:08 -0400

    Adds the ajax methods for loging in

---
+++ frontend/util/user_api_util.js
@@ -0,0 +1,34 @@
+export const signup = (user) => {
+
+    // debugger
+    return (
+        $.ajax({
+            method: "POST",
+            url: "api/user",
+            data: {
+                user
+            }
+        })
+    );
+};
+
+export const login = (user) => {
+    return (
+        $.ajax({
+            method: "POST",
+            url: "api/session",
+            data: {
+                user
+            }
+        })
+    );
+};
+
+export const logout = () => {
+    return (
+        $.ajax({
+            method: "DELETE",
+            url: "api/session",
+        })
+    );
+};
