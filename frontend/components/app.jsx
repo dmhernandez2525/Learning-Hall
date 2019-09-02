175af3c1046e8e79226b4cf6207d25a8879d7ae5
Daniel Hernandez <danielmark252525@gmail.com>
2019-09-02 17:44:52 -0400

    Adds the thunk action creaters and the root for the app

---
+++ frontend/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+
+const App = () => (
+  <div>
+    <h1>Hello Welcome to the Learning Hall</h1>
+    <Switch>
+      <Route exact path="/" component={} />
+    </Switch>
+  </div>
+);
+
+export default App;
