449c012ecfd7a7aade3a5e79137cf9bc88ae7272
Daniel Hernandez <danielmark252525@gmail.com>
2019-09-06 13:52:52 -0400

    first commit to heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require rails-ujs
 //= require activestorage
+//= jquery
 //= require_tree .
 
 
