0530f17d530a1b2dea2a62142059c962ee12c0b4
Daniel Hernandez <danielmark252525@gmail.com>
2019-09-08 00:20:10 -0400

    Adds tasks thunk action creators

---
+++ frontend/actions/task.js
@@ -0,0 +1,44 @@
+import * as APItask from "../util/task"
+export const RECEIVE_ALL_TASKS = "RECEIVE_ALL_TASKS";
+export const RECEIVE_TASK = "RECEIVE_TASK";
+export const DELETE_TASK = "DELETE_TASK";
+
+
+export const allTasks = () => dispatch => (
+    APIsubject.allTasks().then(subjects => dispatch({
+        type: RECEIVE_ALL_TASKS,
+        subjects
+    }))
+);
+
+
+export const newTask = (task) => dispatch => (
+    APItask.newTask(task).then(task => dispatch({
+        type: RECEIVE_TASK,
+        task
+    }))
+);
+
+
+export const showTask = (id) => dispatch => (
+    APItask.showTask(id).then(task => dispatch({
+        type: RECEIVE_TASK,
+        task
+    }))
+);
+
+
+export const updateTask = (task) => dispatch => (
+    APItask.updateTask(task).then(task => dispatch({
+        type: RECEIVE_TASK,
+        task
+    }))
+);
+
+
+export const updateTask = (id) => dispatch => (
+    APItask.updateTask(id).then(taskId => dispatch({
+        type: DELETE_TASK,
+        taskId
+    }))
+);
