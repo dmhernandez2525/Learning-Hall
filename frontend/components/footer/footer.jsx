e6a2b3e6429a6cf16262e6bf445a592c933f364f
Daniel Hernandez <danielmark252525@gmail.com>
2019-09-11 16:42:37 -0400

    styles signUp

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,26 +5,33 @@ const Footer = () => (
     <footer className="footer">
         
         <Link className="logo" to="/" ><img src={window.logoUrl} alt="Learning Hall Logo"></img></Link>        
-    
-        <div className="nav-links">
 
-            <Link to="/">About</Link>
+        <div className="footer-div">
             
-            <Link to="/signIn">Sign In</Link>
+            <div className="nav-links">
+                <h4>COMPANY</h4>
+                    <Link to="/">About</Link>
+                    
+                    <Link to="/signIn">Sign In</Link>
 
-            <Link to="/signUp">Sign Up</Link>
+                    <Link to="/signUp">Sign Up</Link>
 
+                </div>
+                <div className="nav-links">
+                    <h4>PROGRAMS</h4>
+
+                    <Link to="/">About</Link>
+                    <Link to="/signIn">Sign In</Link>
+                    <Link to="/signUp">Sign Up</Link>
+                </div>
+                <div className="nav-links">
+                    <h4>CONNECT</h4>
+                    <Link to="/">About</Link>
+                    <Link to="/signIn">Sign In</Link>
+                    <Link to="/signUp">Sign Up</Link>
+                </div>
         </div>
-        <div className="nav-links">
-            <Link to="/">About</Link>
-            <Link to="/signIn">Sign In</Link>
-            <Link to="/signUp">Sign Up</Link>
-        </div>
-        <div className="nav-links">
-            <Link to="/">About</Link>
-            <Link to="/signIn">Sign In</Link>
-            <Link to="/signUp">Sign Up</Link>
-        </div>
+ 
     </footer>
 )
 
--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -47,10 +47,16 @@ class SignUp extends React.Component {
 
 
                 {Errors}
+                {/* <div className="g-signin2" data-width="400" data-height="40px" data-longtitle="true" data-onsuccess="onSignIn"></div> */}
+
                 <form className="auth_form" onSubmit={this.handleSumbit}>
-                    {/* <h2 className="formH2"> Sign up to start Learning </h2> */}
-                    <h2 > Sign up to start Learning </h2>
-                    {/* <label >Username */}
+                    <div className="auth_form_top">                    
+                        <h2 > Sign up to start Learning </h2>
+                        <h3 > Create your account </h3>
+
+                    </div>
+
+                 
                     <input
                         className="auth-big-input"
                         type="text"
@@ -63,7 +69,7 @@ class SignUp extends React.Component {
                     {/* <label >Email */}
                     <input
                         className="auth-big-input"
-                        type="text"
+                        type="email"
                         value={this.state.email}
                         placeholder="Email"
                         onChange={this.handleInput("email")}
@@ -120,7 +126,6 @@ class SignUp extends React.Component {
 
 
                     </div>
-                        <div className="g-signin2" data-onsuccess="onSignIn"></div>
 
                 </form>
 
@@ -135,3 +140,94 @@ class SignUp extends React.Component {
 
 
 export default SignUp;
+
+
+
+
+
+
+
+
+
+
+/* <html>
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+  <script src="https://apis.google.com/js/api:client.js"></script>
+  <script>
+  var googleUser = {};
+  var startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById('customBtn'));
+    });
+  };
+
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+          document.getElementById('name').innerText = "Signed in: " +
+              googleUser.getBasicProfile().getName();
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }
+  </script>
+  <style type="text/css">
+    #customBtn {
+      display: inline-block;
+      background: white;
+      color: #444;
+      width: 190px;
+      border-radius: 5px;
+      border: thin solid #888;
+      box-shadow: 1px 1px 1px grey;
+      white-space: nowrap;
+    }
+    #customBtn:hover {
+      cursor: pointer;
+    }
+    span.label {
+      font-family: serif;
+      font-weight: normal;
+    }
+    span.icon {
+      background: url('/identity/sign-in/g-normal.png') transparent 5px 50% no-repeat;
+      display: inline-block;
+      vertical-align: middle;
+      width: 42px;
+      height: 42px;
+    }
+    span.buttonText {
+      display: inline-block;
+      vertical-align: middle;
+      padding-left: 42px;
+      padding-right: 42px;
+      font-size: 14px;
+      font-weight: bold;
+     // Use the Roboto font that is loaded in the <head> 
+font - family: 'Roboto', sans - serif;
+    }
+  </style >
+  </head >
+    <body>
+        
+  <div id="gSignInWrapper">
+            <span class="label">Sign in with:</span>
+            <div id="customBtn" class="customGPlusSignIn">
+                <span class="icon"></span>
+                <span class="buttonText">Google</span>
+            </div>
+        </div>
+        <div id="name"></div>
+        <script>startApp();</script>
+    </body>
+</html >
+*/
