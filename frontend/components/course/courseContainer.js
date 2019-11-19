4dcbba27ea0ebbd184c74e91540db89789fc75cb
Daniel Hernandez <danielmark252525@gmail.com>
2019-11-18 19:21:00 -0500

    Adds Make a New Course

--- frontend/components/course/courseContainer.js
+++ frontend/components/course/courseContainer.js
@@ -5,11 +5,19 @@ const mapStateToProps = (state) => ({
     course: {
         name: "",
         author_id: state.session.currentUser.id,
+        id: "",
+        courseName: "",
+        FormType: "Make a New Course"
+    },
+    allCourse: {
+        courses: Object.values(state.entities.courses)
     }
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    newCourse: (course) => dispatch(newCourse(course))
+    newCourse: (course) => dispatch(newCourse(course)),
+    updateCourse: (course) => dispatch(updateCourse(course)),
+    deleteCourse: (id) => dispatch(deleteCourse(id))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CourseForm)
